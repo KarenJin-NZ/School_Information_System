b449e5035708f5e77419386c8256da2bc09b2e9e
KarenJin-NZ <karenjin0216@gmail.com>
2021-06-10 20:53:18 +1200

    structure

---
+++ School_Information_System/School_Information_System.cpp
@@ -0,0 +1,11 @@
+// School_Information_System.cpp : This file contains the 'main' function. Program execution begins and ends there.
+// Group Assignment - CS103
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Hello World";
+}
