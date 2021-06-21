a5807aef02903eb9dec75b84f4ee69d0838ef2a6
KarenJin-NZ <karenjin0216@gmail.com>
2021-06-22 11:39:31 +1200

    Updated codes

---
+++ School_Information_System/Goto.cpp
@@ -0,0 +1,9 @@
+#include <Windows.h>
+
+void Gotoxy(int x, int y)
+{
+	COORD coord;
+	coord.X = x;
+	coord.Y = y;
+	SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), coord);
+}
---
+++ School_Information_System/Goto.h
@@ -0,0 +1,6 @@
+#ifndef GOTO_H
+#define GOTO_H
+
+void Gotoxy(int, int);
+
+#endif
